f6f0ffb8adcb2e6d1a7c17f131cedf5ebb1c69df
Márton Csoma <csoma.m@gmail.com>
2017-09-12 15:57:25 +0200

    SumAll

---
+++ week-02/day-2/src/SumAll.java
@@ -0,0 +1,17 @@
+public class SumAll {
+    public static void main(String[] args) {
+        // - Create an array variable named `abc`
+        //   with the following content: `["first", "second", "third"]`
+        // - Swap the first and the third element of `abc`
+
+        String[] abc = {"first", "second", "third"};
+
+        String temp = abc[0];
+        abc[0] = abc[2];
+        abc[2] = temp;
+
+        for (int i=0; i < abc.length; i++) {
+            System.out.print(abc[i]+ " ");
+        }
+    }
+}

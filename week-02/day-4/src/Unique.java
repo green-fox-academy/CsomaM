cb191c615887ce8219d8d10262a9ca489f27082a
Márton Csoma <csoma.m@gmail.com>
2017-09-14 12:36:14 +0200

    Fixed up unique and bubble, functions are used

--- week-02/day-4/src/Unique.java
+++ week-02/day-4/src/Unique.java
@@ -1,27 +1,32 @@
 import java.util.*;
 
 public class Unique {
+
     public static void main(String[] args) {
-        List numbers = new ArrayList();
-        Scanner sc = new Scanner(System.in);
+
+        ArrayList numbers = new ArrayList();
 
         System.out.println("Give me numbers. I will ask for numbers as long as you don't type *.");
+        Fill(numbers);
+
+        System.out.println(numbers);
+
+    }
+
+    static ArrayList <Integer> Fill (ArrayList list) {
+        Scanner sc = new Scanner(System.in);
         String scan ="";
         int n = 0;
-        int duplicates = 0;
-
         do {
             scan = sc.next();
             if (!scan.equals("*")) {
                 n = Integer.parseInt(scan);
-                if (!numbers.contains(n)) {
-                    numbers.add(n);
+                if (!list.contains(n)) {
+                    list.add(n);
                 }
             }
         } while (!scan.equals("*"));
-
-        System.out.println(numbers);
-
+        return (list);
     }
 
 

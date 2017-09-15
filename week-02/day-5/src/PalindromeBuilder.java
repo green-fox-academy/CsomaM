e07f5d5fe8cb631be6af08916b739287b4aa1fb3
Márton Csoma <csoma.m@gmail.com>
2017-09-15 13:01:35 +0200

    PalindromeBuilder

---
+++ week-02/day-5/src/PalindromeBuilder.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class PalindromeBuilder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gime me a string and I'll make it a palindrome.");
+        String input = sc.nextLine();
+        System.out.println(palGen(input));
+    }
+
+    static String palGen (String input) {
+        String temp = new StringBuffer(input).reverse().toString();
+        String palindrome = input + temp;
+        return (palindrome);
+    }
+}

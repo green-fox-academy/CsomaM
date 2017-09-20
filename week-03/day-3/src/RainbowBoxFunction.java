41c82be56442fe67f72fc30db95c3b37f31648dc
Márton Csoma <csoma.m@gmail.com>
2017-09-20 12:09:39 +0200

    added rainbow rectangle

--- week-03/day-3/src/RainbowBoxFunction.java
+++ week-03/day-3/src/RainbowBoxFunction.java
@@ -11,8 +11,14 @@ public class RainbowBoxFunction {
       // the square size, and the fill color,
       // and draws a square of that size and color to the center of the canvas.
       // create a loop that fills the canvas with rainbow colored squares.
+        drawSquare(graphics, 200, 255, 0,0);
+        drawSquare(graphics, 150, 0,255,0);
+        drawSquare(graphics, 100, 0,0,255);
+    }
 
-
+    static void drawSquare(Graphics g1, int size, int r, int g, int b) {
+        g1.setColor(new  Color (r,g,b));
+        g1.fillRect(160-size/2, 171-size/2, size, size);
 
     }
 

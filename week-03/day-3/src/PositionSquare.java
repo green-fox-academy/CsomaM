ce70c40ca1f33e7f80126e53ab2f8b9330e07400
Márton Csoma <csoma.m@gmail.com>
2017-09-20 11:41:21 +0200

    added PositionSquare

--- week-03/day-3/src/PositionSquare.java
+++ week-03/day-3/src/PositionSquare.java
@@ -6,17 +6,22 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class PositionSquare {
 
-    public static void mainDraw(Graphics graphics){
-      // create a square drawing function that takes 2 parameters:
-      // the x and y coordinates of the square's top left corner
-      // and draws a 50x50 square from that point.
-      // draw 3 squares with that function.
-
-
-
+    public static void mainDraw(Graphics graphics) {
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        drawSquare(graphics, 10, 10);
+        drawSquare(graphics, 15, 30);
+        drawSquare(graphics, 200, 100);
+        drawSquare(graphics, 50, 5);
     }
 
-//    Don't touch the code below
+    static void drawSquare(Graphics g, int x, int y) {
+        g.drawRect(x, y, 50, 50);
+    }
+
+    //    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
 
@@ -28,7 +33,8 @@ public class PositionSquare {
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
     }
-    static class ImagePanel extends JPanel{
+
+    static class ImagePanel extends JPanel {
         @Override
         protected void paintComponent(Graphics graphics) {
             super.paintComponent(graphics);
@@ -36,5 +42,4 @@ public class PositionSquare {
 
         }
     }
-
 }

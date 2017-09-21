acd06965da93fb54c6c41531fb797db2d69d9cb9
Márton Csoma <csoma.m@gmail.com>
2017-09-21 11:07:33 +0200

    Bunnies

--- week-03/day-4/src/Bunny1.java
+++ week-03/day-4/src/Bunny1.java
@@ -1,2 +1,22 @@
 // We have a number of bunnies and each bunny has two big floppy ears.
 // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+import java.util.Scanner;
+
+public class Bunny1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give me the number of bunnies.");
+        int n = sc.nextInt();
+        System.out.println("The number of ears: " + earCounter(n));
+    }
+
+    static int earCounter(int n) {
+        int ears = 0;
+        if ((n) == 0) {
+            return 0;
+        } else {
+            return ears + 2 + earCounter(n - 1);
+        }
+    }
+}
--- week-03/day-4/src/Bunny2.java
+++ week-03/day-4/src/Bunny2.java
@@ -2,3 +2,28 @@
 // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 // have 3 ears, because they each have a raised foot. Recursively return the
 // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+import java.util.Scanner;
+
+public class Bunny2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give me the number of bunnies.");
+        int n = sc.nextInt();
+        System.out.println("The number of ears: " + earCounter(n));
+    }
+
+    static int earCounter(int n) {
+        int ears = 0;
+        if ((n) == 0) {
+            return 0;
+        } else {
+            if (n % 2 == 0) {
+                return ears + 2 + earCounter(n - 1);
+            } else {
+                return ears + 3 + earCounter(n - 1);
+            }
+        }
+    }
+}

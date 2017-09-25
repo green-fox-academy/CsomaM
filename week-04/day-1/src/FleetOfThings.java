426c12fb6aeebce5f6d775f5c5e6294b62bf35e3
Márton Csoma <csoma.m@gmail.com>
2017-09-25 15:04:38 +0200

    added fleet of things

---
+++ week-04/day-1/src/FleetOfThings.java
@@ -0,0 +1,23 @@
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+        Thing first = new Thing("Get milk");
+        Thing second = new Thing("Remove the obstacles");
+        Thing third = new Thing("Stand up");
+        third.complete();
+        Thing fourth = new Thing("Eat lunch");
+        fourth.complete();
+        fleet.add(first);
+        fleet.add(second);
+        fleet.add(third);
+        fleet.add(fourth);
+
+        System.out.println(fleet);
+    }
+}

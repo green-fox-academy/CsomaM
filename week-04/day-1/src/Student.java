53c554835fae017774334715c03159c90f24e8c2
Márton Csoma <csoma.m@gmail.com>
2017-09-25 16:08:56 +0200

    added teacher, student

---
+++ week-04/day-1/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+    Teacher teacher = new Teacher();
+
+    public void learn() {
+    }
+
+    public void question() {
+        teacher.answer();
+    }
+}
---
+++ week-04/day-1/src/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+
+    Student student = new Student();
+
+    public void teach() {
+        student.learn();
+    }
+
+    public void answer() {
+    }
+}

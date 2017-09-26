764415660c421bcb780169b1953516b68d4a38cb
Márton Csoma <csoma.m@gmail.com>
2017-09-26 14:00:08 +0200

    added Person class

---
+++ week-04/day-2/src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    protected String name;
+    protected int age;
+    protected String gender;
+
+    public Person (String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Person () {
+        this.name = "Jane Doe";
+        this.age = 30;
+        this.gender = "Female";
+    }
+
+    public void introduction () {
+        System.out.println("Hi, I'm " + name + ", " + age + " year old " + gender);
+    }
+
+    public void getGoal () {
+        System.out.println("My goal is: Live for the moment!");
+    }
+
+}

7db659e6872da172c54559addbc9efd4ad92d9ba
Márton Csoma <csoma.m@gmail.com>
2017-10-25 11:44:49 +0200

    added a non-working default page

--- week-07/day-3/Bank of Simba/src/main/java/com/greenfoxacademy/bank/controller/DefaultController.java
+++ week-07/day-3/Bank of Simba/src/main/java/com/greenfoxacademy/bank/controller/DefaultController.java
@@ -3,12 +3,17 @@ package com.greenfoxacademy.bank.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DefaultController {
 
-    @RequestMapping("/")
+    @RequestMapping("")
     public String defaultPage (Model model) {
+        String str = "Welcome to my webpage!";
+        String path = "Throw.gif";
+        model.addAttribute("welcomeMSG" , str);
+        model.addAttribute("path" , path);
         return "default";
     }
 }
